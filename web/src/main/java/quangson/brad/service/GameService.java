b935133501eedd9f1bb987aeb7f521972162e63c
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2021-03-11 12:47:04 -0700

    add WebConfig and GameServices

---
+++ web/src/main/java/quangson/brad/service/GameService.java
@@ -0,0 +1,10 @@
+package quangson.brad.service;
+
+public interface GameService {
+
+    boolean isGameOver();
+    String getMainMessage();
+    String getResultMessage();
+    void checkGuess(int guess);
+    void reset();
+}
---
+++ web/src/main/java/quangson/brad/util/ViewNames.java
@@ -0,0 +1,8 @@
+package quangson.brad.util;
+
+public final class ViewNames {
+
+    public static final String HOME = "home";
+
+    private ViewNames(){}
+}
